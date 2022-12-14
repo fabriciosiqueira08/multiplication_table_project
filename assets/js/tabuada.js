9b38f4888d2c2a97611efd9c268fff7e01e60191
fabriciosiqueira08 <henriquefabricio008@gmail.com>
2022-12-14 15:17:24 -0300

    Adicionado for na tabuada

---
+++ assets/js/tabuada.js
@@ -0,0 +1,24 @@
+let tabuada = document.querySelector("#tabuada tbody");
+let valorA = 5;
+
+tabuada.innerHTML = '';
+
+for (let valorB = 0; valorB <= 10; valorB++) {
+
+    let resultado = valorA * valorB;
+
+    let template = `   
+        <td>${valorA}</td>
+        <td>x</td>
+        <td>${valorB}</td>
+        <td>=</td>
+        <td>${resultado}</td>
+    `;
+
+    let tr = document.createElement("tr");
+
+    tr.innerHTML = template;
+
+    tabuada.append(tr);
+
+}
